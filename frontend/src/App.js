63c082b83e45798aa27eb0ca3044b2c292d43a45
israelmarmar <email.israel.br@gmail.com>
2020-03-27 16:03:13 -0400

    testes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Routes from './routes';
-
-
-import './global.css';
+import Logon form './pages/Logon'
 
 function App() {
   return (
-    <Routes />
+    <Logon
   );
 }
 
